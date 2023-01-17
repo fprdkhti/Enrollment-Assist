ae521673f6dc429842cfb702d82d648d641499a6
Fateme Pardakhti <f.pk844@gmail.com>
2023-01-18 00:54:40 +0330

    Update and rename stepDefenition.java to stepDefinition.java

---
+++ src/test/java/cucumber/stepDefinition.java
@@ -0,0 +1,57 @@
+package cucumber;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class stepDefinition {
+
+    @Given("title is null")
+    public void title_is_null() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("ask for creating course")
+    public void ask_for_creating_course() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("should throw {string}")
+    public void should_throw(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("should be able to")
+    public void shouldBeAbleTo() {
+        
+    }
+
+    @When("want to take {string} and {string}")
+    public void wantToTakeAnd(String arg0, String arg1) {
+        
+    }
+
+    @Given("student {string} is existed")
+    public void studentIsExisted(String arg0) {
+    }
+
+    @Then("should not be able to")
+    public void shouldNotBeAbleTo() {
+        
+    }
+
+    @Given("one course with course number {string} and title {string} and credit {string} and graduate level {string}")
+    public void oneCourseWithCourseNumberAndTitleAndCreditAndGraduateLevel(String arg0, String arg1, String arg2, String arg3) {
+        
+    }
+
+    @When("add new course")
+    public void addNewCourse() {
+    }
+}
